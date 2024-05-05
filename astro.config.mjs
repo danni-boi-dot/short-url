8a9051b656b5e6cd4a6b3fe31e4b584d9a47542a
daniel <adtdaniel87@gmail.com>
2024-05-05 17:29:39 -0600

    instalacion de tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from "astro/config";
 import db from "@astrojs/db";
 import node from "@astrojs/node";
-
 import auth from "auth-astro";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db(), auth()],
+  integrations: [db(), auth(), tailwind()],
   output: "server",
   adapter: node({
-    mode: "standalone",
-  }),
+    mode: "standalone"
+  })
 });
