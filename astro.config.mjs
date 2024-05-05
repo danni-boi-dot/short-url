7bc0ecdcfe0a43e561ab99ef7b7356dd5b030809
daniel <adtdaniel87@gmail.com>
2024-05-05 14:23:40 -0600

    Configured OAuth

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,26 @@
-import { defineConfig } from 'astro/config';
+import Google from "@auth/core/providers/google";
+import GitHub from "@auth/core/providers/github";
+import { defineConfig } from "astro/config";
 import db from "@astrojs/db";
-
 import node from "@astrojs/node";
+
+import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db()],
+  providers: [
+    GitHub({
+      clientId: import.meta.env.GITHUB_CLIENT_ID,
+      clientSecret: import.meta.env.GITHUB_CLIENT_SECRET,
+    }),
+    Google({
+      clientId: import.meta.env.GOOGLE_CLIENT_ID,
+      clientSecret: import.meta.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  integrations: [db(), auth()],
   output: "server",
   adapter: node({
-    mode: "standalone"
-  })
+    mode: "standalone",
+  }),
 });
